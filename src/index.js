88f4fcbc36db490c105dde21d2db83d3c6ed7038
mazdak <nazemi.works@gmail.com>
2019-05-15 22:23:18 -0700

    starter of websocket socket.io client/server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+//normally express do this behind scene
+const server = http.createServer(app); //refactoring for socket 
+//instance of socketio -- we make server to can explicity pass here
+const io = socketio(server); // its app of socketio like express
+
 const port = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname,'../public');
 app.use(express.static(publicDirPath));
 
-// app.get('/',(req,res)=>{
-//   res.send('./index.html');
-// })
+io.on('connection',()=>{
+  console.log('new websockt connection');
+}); //after this initiation we need run client side.
 
 
 
-
-app.listen(port,()=>{
+server.listen(port,()=>{
   console.log(`running on port ${port}`);
 })
