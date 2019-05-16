f7ee194ba183429ecf0c45ba28faa5a50fa84f41
mazdak <nazemi.works@gmail.com>
2019-05-15 22:56:12 -0700

    socket connection theme from server to client and vv emit and on

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,11 @@
-io();
+//allow to send and receive
+const socket = io();
+//receiving from server
+socket.on('countUpdated',(count)=>{
+  console.log('en updated' , count);
+})
+//now client send data to server
+document.querySelector('#increment').addEventListener('click',()=>{
+  console.log('CXClikcd');
+  socket.emit('increment'); // send to server now must make sure sr get it
+});
